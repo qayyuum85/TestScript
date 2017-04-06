69d6e54854cf739c3648c76e77ebc3e890b89237
Qayyuum <qayyuum.harun@gmail.com>
2017-04-06 17:22:19 +0800

    Update to make the stream run forever

--- TwitterStreamtoCacheWIBIS.py
+++ TwitterStreamtoCacheWIBIS.py
@@ -3,6 +3,7 @@ import codecs, sys, json, time, os
 import tweepy
 from tweepy import Stream
 from tweepy.streaming import StreamListener
+from twTOM import getprops
 import twTOM
 import intersys.pythonbind3
 
@@ -10,73 +11,81 @@ import intersys.pythonbind3
 # Please hardcode this value in case you are running from the notebook
 configId = 2
 secrets = getprops("WIBIS.properties")
-user = secrets['USERNAME'];
-password = secrets['PASSWORD'];
-host = secrets['HOST'];
-port = secrets['PORT'];
+user = secrets['USERNAME']
+password = secrets['PASSWORD']
+host = secrets['HOST']
+port = secrets['PORT']
+ns = 'SMSWIBIS'
 
-try:
-    class listener(StreamListener):
-        def on_data(self, data):
-            d = json.loads(data)
-            print(d['text'])
-            twMsg = database.create_new("%BI.TweetMessage", None)
-            twMsg.set("DateCreated", d['created_at'])
-            twMsg.set("TweetMessage", d['text'])
-            twMsg.set("Username", d['user']['screen_name'])
-            twMsg.set("TweetID", d['id_str'])
+print('Running Twitter Streaming Service...')
+print('You can interrupt this process by pressing Ctrl-C or Delete. It will take a few moment before the service is stopped.')
+print('You can rerun this again by pasting this command:')
+print(r'python C:\InterSystems\Cache\CSP\sys\bi\python\TwitterStreamtoCacheWIBIS.py')
 
-            urls = d['entities']['urls']
-            for url in urls:
-                twMsg.run_obj_method("AddLink", [url['expanded_url']])
+class listener(StreamListener):
+    def on_data(self, data):
+        d = json.loads(data)
+        print(d['text'])
+        twMsg = database.create_new("%BI.TweetMessage", None)
+        twMsg.set("DateCreated", d['created_at'])
+        twMsg.set("TweetMessage", d['text'])
+        twMsg.set("Username", d['user']['screen_name'])
+        twMsg.set("TweetID", d['id_str'])
 
-            twMsg.run_obj_method("%Save",[])
-            return True
+        urls = d['entities']['urls']
+        for url in urls:
+            twMsg.run_obj_method("AddLink", [url['expanded_url']])
 
-        def on_error(self, status):
-            print (status)
+        twMsg.run_obj_method("%Save",[])
+        return True
 
-    # Connect to specified machine, in the SAMPLES namespace
-    url = host + "["+port+"]:" + ns
-    conn = intersys.pythonbind3.connection()
-    conn.connect_now(url, user, password, None)
-    database = intersys.pythonbind3.database(conn)
+    def on_error(self, status):
+        print (status)
+        time.sleep(60)
+        return True
 
-    # Get filter based on WebExtractor setting
-    trackFilter = database.run_class_method("%BI.WebExtractor", "GetFilter", [configId])
-    # Get authentication from file
-    auth = twTOM.Authenticate()
-    twitterStream = Stream(auth, listener())
-    twitterStream.filter(track=trackFilter, languages=['en'])
-    processId = os.getpid()
+# Connect to specified machine, in the SAMPLES namespace
+url = host + "["+port+"]:" + ns
+conn = intersys.pythonbind3.connection()
+conn.connect_now(url, user, password, None)
+database = intersys.pythonbind3.database(conn)
 
-# various exception handling blocks
-except KeyboardInterrupt:
-    sys.exit()
+# Get filter based on WebExtractor setting
+trackFilter = database.run_class_method("%BI.WebExtractor", "GetFilter", [configId])
 
-except AttributeError as e:
-    print('AttributeError was returned')
-    pass
+# Get authentication from file
+auth = twTOM.Authenticate()
 
-except tweepy.TweepError as e:
-    print(e)
-    if '401' in e:
-        # not sure if this will even work
+while True:
+    try:
+        twitterStream = Stream(auth, listener(), timeout=60)
+        twitterStream.filter(track=trackFilter, languages=['en'])
+        processId = os.getpid()
+
+    # various exception handling blocks
+    except KeyboardInterrupt:
+        print('Keyboard Interrupt detected. Ending service...')
+        sys.exit()
+
+    except AttributeError as e:
+        print('AttributeError was returned')
+        time.sleep(60)
+        pass
+
+    except tweepy.TweepError as e:
         print('Below is the response that came in')
         print(e)
         time.sleep(60)
         pass
-    else:
-        #raise an exception if another status code was returned, we don't like other kinds
-        raise e
 
-except intersys.pythonbind3.cache_exception as err:
-    print ("InterSystems Cache' exception")
-    print (sys.exc_type)
-    print (sys.exc_value)
-    print (sys.exc_traceback)
-    print ('Error code:' + str(err))
+    except intersys.pythonbind3.cache_exception as err:
+        print ("InterSystems Cache' exception")
+        print(err)
+        time.sleep(60)
+        pass
 
-except Exception as e:
-    print('Unhandled exception')
-    raise e
+    except Exception as e:
+        print('Unhandled exception')
+        print(e)
+        time.sleep(60)
+        pass
